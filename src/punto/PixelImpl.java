6ea1c53da2cd59d1a88fc6e2e76c88a1a3817db7
Enric Climent <enric612@gmail.com>
2014-04-12 15:59:32 +0200

    Creados constructores Pixel

--- src/punto/PixelImpl.java
+++ src/punto/PixelImpl.java
@@ -4,6 +4,24 @@ public class PixelImpl extends PuntoImpl implements Pixel {
 
 	private Color color;
 	
+	public PixelImpl(){
+		super();
+		this.color = Color.VERDE;
+	}
+	
+	public PixelImpl(Color c){
+		super();
+		this.color = c;
+	}
+	
+	public PixelImpl(Double x, Double y,Color c) {
+		super(x,y);
+		this.color = c;
+		
+		
+		
+	}
+	
 	@Override
 	public Color getColor() {
 		return this.color;
